8c2f21dfb1fe82ed7de10221635e4f1ae206f172
thetyper101 <ewcgong@hotmail.com>
2023-09-19 20:50:15 -0400

    did some stuff with header demo

--- src/responses.js
+++ src/responses.js
@@ -1,4 +1,4 @@
-const fs = require('fs');  // pull in the file system module
+const fs = require('fs'); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
@@ -8,11 +8,25 @@ const respond = (request, response, content, type) => {
   response.end();
 };
 
+const getCats = (request, response, acceptedTypes) => {
+  const cat = { name: 'Mr. Cat', age: 8 };
+  // Send xml if requested.
+  if (acceptedTypes[0] === 'text/xml') {
+    let xmlResponse = '<response>';
+    xmlResponse = `${xmlResponse}<name>${cat.name}</name>`;
+    xmlResponse = `${xmlResponse}<age>${cat.age}</age>`;
+    xmlResponse = `${xmlResponse}/response>`;
+    return respond(request, response, xmlResponse, acceptedTypes[0]);
+  }
+  // By default, server response is in JSON.
+  const catString = JSON.stringify(cat);
+  return respond(request, response, catString, 'application/json');
+};
+
 const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
 };
 
 module.exports = {
-  getCats,
-  getIndex,
+  getIndex, getCats,
 };
--- src/server.js
+++ src/server.js
@@ -5,13 +5,23 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-
+  '/cats': responseHandler.getCats,
+  '/': responseHandler.getIndex,
+  index: responseHandler.getIndex,
 };
 
 const onRequest = (request, response) => {
+  const parsedURL = url.parse(request.url);
+  const acceptedTypes = request.headers.accept.split(',');
 
+  const handlerFunction = urlStruct[parsedURL.pathname];
+  if (handlerFunction) {
+    handlerFunction(request, response, acceptedTypes);
+  } else {
+    urlStruct.index(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
+  console.log(`Listening on 127.0.0.1: ${port}`);
 });
